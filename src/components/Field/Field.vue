b6c05dce0100a6aa1f441137836b35c9bc196c90
Martin <mr.mmaurer+work@gmail.com>
2019-11-17 01:23:13 +0000

    Added email and text validation

--- src/components/Field/Field.vue
+++ src/components/Field/Field.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 // import { Input } from '@/components/types.ts';
 
 @Component
@@ -26,7 +26,9 @@ export default class Field extends Vue {
   @Prop() private name!: string;
   @Prop() private label!: string;
   @Prop() private value!: string;
-  @Prop({ default: false }) private isInvalid!: boolean;
+  @Prop() private type!: 'text' | 'email';
+
+  private isInvalid: boolean = true;
 
   handleInput(event: Event): void {
     if (!event) {
@@ -34,10 +36,40 @@ export default class Field extends Vue {
     }
 
     const { target } = event;
-    const newValue: string = (target as HTMLInputElement).value;
+    const newValue = (target as HTMLInputElement).value;
 
-    console.log(newValue);
     this.$emit('input', newValue);
+  }
+
+  static isValidTextField(fieldValue: string): boolean {
+    return fieldValue.length > 0;
+  }
+
+  static isValidEmailField(fieldValue: string): boolean {
+    // https://emailregex.com/
+    const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    const isValid = emailRegex.test(fieldValue);
+
+    return isValid;
+  }
+
+  @Watch('value')
+  onValueChange(newValue: string) {
+    let newIsInvalid = this.isInvalid;
+
+    if (this.type === 'text') {
+      const validity = Field.isValidTextField(newValue);
+
+      newIsInvalid = !validity;
+    }
+
+    if (this.type === 'email') {
+      const validity = Field.isValidEmailField(newValue);
+
+      newIsInvalid = !validity;
+    }
+
+    this.isInvalid = newIsInvalid;
   }
 }
 </script>
