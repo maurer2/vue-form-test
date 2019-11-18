4189cedcda3d7e4a6aea2c09d2314e508e83361f
Martin <mr.mmaurer+work@gmail.com>
2019-11-18 00:36:31 +0000

    Added tests for Summary component

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testMatch: [
+    '**/*.spec.(js|ts)|**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+  ],
 };
