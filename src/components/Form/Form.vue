57ccbdd594327f997f2db2a7ca6ec79067cb679a
Martin <mr.mmaurer+work@gmail.com>
2019-11-19 22:04:46 +0000

    Moved fiields to formdata

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -41,6 +41,7 @@
 import { Vue, Component } from 'vue-property-decorator';
 import { FieldType } from '@/types.ts';
 import Field from '../Field/Field.vue';
+import formdata from './formdata';
 
 @Component({
   components: {
@@ -48,48 +49,7 @@ import Field from '../Field/Field.vue';
   },
 })
 export default class Form extends Vue {
-  private fields: FieldType[] = [
-    {
-      value: '',
-      name: 'First name',
-      id: 'firstName',
-      label: 'First Name',
-      type: 'text',
-      placeholder: 'Please enter your first name',
-      isValid: false,
-      isRequired: true,
-    },
-    {
-      value: '',
-      name: 'Last name',
-      id: 'lastName',
-      label: 'Last name',
-      type: 'text',
-      placeholder: 'Please enter your last name',
-      isValid: false,
-      isRequired: true,
-    },
-    {
-      value: '',
-      name: 'Email',
-      id: 'email',
-      label: 'Email',
-      type: 'email',
-      placeholder: 'Please enter your email',
-      isValid: false,
-      isRequired: true,
-    },
-    {
-      value: '',
-      name: 'Customer Query',
-      id: 'customerQuery',
-      label: 'Customer query',
-      type: 'textarea',
-      placeholder: 'Please enter your customer query',
-      isValid: true,
-      isRequired: true,
-    },
-  ];
+  private fields = formdata;
 
   get isSubmittable(): boolean {
     const allFieldsAreValid = this.fields.every((field: FieldType) => field.isValid);
